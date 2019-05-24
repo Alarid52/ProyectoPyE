bb6ffa4715c40a5755eecc22fb6ebfff129ff3ce
MarvinJimenez13 <marvinjiga@gmail.com>
2019-05-23 22:12:05 -0500

    Primera versión, sólo Menu.

---
+++ src/proyectopye/ProyectoPyE.java
@@ -0,0 +1,13 @@
+
+package proyectopye;
+
+import JFrames.Menu;
+
+public class ProyectoPyE {
+
+    public static void main(String[] args) {
+        Menu menu = new Menu();
+        menu.setVisible(true);
+    }
+    
+}
