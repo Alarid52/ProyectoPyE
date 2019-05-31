bdddf7c4ef374c09e73198f25c0dd6c6e24a5ca5
MarvinJimenez13 <marvinjiga@gmail.com>
2019-05-31 17:53:44 -0500

    Nueva version. integral dx.

--- src/JFrames/Menu.java
+++ src/JFrames/Menu.java
@@ -15,15 +15,22 @@ public class Menu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        FuncionD = new javax.swing.JLabel();
-        LimiteXY = new javax.swing.JLabel();
-        ValorE = new javax.swing.JLabel();
-        PLimitesV = new javax.swing.JLabel();
-        Covarianza = new javax.swing.JLabel();
+        jRadioButton1 = new javax.swing.JRadioButton();
+        jButton1 = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jTextField3 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
         ProyectoPYE = new javax.swing.JLabel();
         cerrarButton = new javax.swing.JLabel();
         MinButton = new javax.swing.JLabel();
         fondoLabel = new javax.swing.JLabel();
+
+        jRadioButton1.setText("jRadioButton1");
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(0, 51, 51));
@@ -32,55 +39,58 @@ public class Menu extends javax.swing.JFrame {
         setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        FuncionD.setFont(new java.awt.Font("Gloucester MT Extra Condensed", 1, 24)); // NOI18N
-        FuncionD.setForeground(new java.awt.Color(51, 51, 51));
-        FuncionD.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        FuncionD.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icons8_Calculator_32px.png"))); // NOI18N
-        FuncionD.setText("Función de Distribución Lineal");
-        FuncionD.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        FuncionD.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        getContentPane().add(FuncionD, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 80, 270, -1));
+        jButton1.setText("CALCULAR");
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 260, -1, -1));
 
-        LimiteXY.setFont(new java.awt.Font("Gloucester MT Extra Condensed", 1, 24)); // NOI18N
-        LimiteXY.setForeground(new java.awt.Color(51, 51, 51));
-        LimiteXY.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        LimiteXY.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icons8_Calculator_32px.png"))); // NOI18N
-        LimiteXY.setText("Límite en X y en Y");
-        LimiteXY.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        LimiteXY.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        getContentPane().add(LimiteXY, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 130, -1, -1));
+        jLabel5.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        jLabel5.setText("RESULTADO");
+        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 260, 120, 30));
 
-        ValorE.setFont(new java.awt.Font("Gloucester MT Extra Condensed", 1, 24)); // NOI18N
-        ValorE.setForeground(new java.awt.Color(51, 51, 51));
-        ValorE.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        ValorE.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icons8_Calculator_32px.png"))); // NOI18N
-        ValorE.setText("Valor Esperado en Mx y My");
-        ValorE.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        ValorE.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        getContentPane().add(ValorE, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 180, -1, -1));
+        jLabel4.setFont(new java.awt.Font("Bodoni MT Condensed", 2, 36)); // NOI18N
+        jLabel4.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel4.setText("dx");
+        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 150, 30, 50));
 
-        PLimitesV.setFont(new java.awt.Font("Gloucester MT Extra Condensed", 1, 24)); // NOI18N
-        PLimitesV.setForeground(new java.awt.Color(51, 51, 51));
-        PLimitesV.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        PLimitesV.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icons8_Calculator_32px.png"))); // NOI18N
-        PLimitesV.setText("Probabilidad para Límites Variables");
-        PLimitesV.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        PLimitesV.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        getContentPane().add(PLimitesV, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 230, -1, -1));
+        jTextField1.setFont(new java.awt.Font("Bodoni MT Condensed", 2, 14)); // NOI18N
+        jTextField1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 130, 20, 20));
 
-        Covarianza.setFont(new java.awt.Font("Gloucester MT Extra Condensed", 1, 24)); // NOI18N
-        Covarianza.setForeground(new java.awt.Color(51, 51, 51));
-        Covarianza.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        Covarianza.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icons8_Calculator_32px.png"))); // NOI18N
-        Covarianza.setText("Covarianza");
-        Covarianza.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        Covarianza.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
-        getContentPane().add(Covarianza, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 280, -1, -1));
+        jTextField3.setFont(new java.awt.Font("Bodoni MT Condensed", 2, 24)); // NOI18N
+        jTextField3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField3ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jTextField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 157, 130, 40));
 
-        ProyectoPYE.setFont(new java.awt.Font("Gloucester MT Extra Condensed", 1, 20)); // NOI18N
+        jTextField2.setFont(new java.awt.Font("Bodoni MT Condensed", 2, 14)); // NOI18N
+        jTextField2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField2ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(175, 200, 20, 20));
+
+        jLabel1.setBackground(new java.awt.Color(0, 0, 0));
+        jLabel1.setFont(new java.awt.Font("Dialog", 1, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel1.setText("=");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 160, -1, -1));
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/int.jpg"))); // NOI18N
+        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 140, -1, 70));
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/integral.png"))); // NOI18N
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 140, 150, 70));
+
+        ProyectoPYE.setFont(new java.awt.Font("Gloucester MT Extra Condensed", 1, 30)); // NOI18N
         ProyectoPYE.setForeground(new java.awt.Color(51, 51, 51));
         ProyectoPYE.setText("Proyecto Probabilidad y Estadística");
-        getContentPane().add(ProyectoPYE, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 40, -1, -1));
+        getContentPane().add(ProyectoPYE, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 40, 340, 60));
 
         cerrarButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icons8_Multiply_32px.png"))); // NOI18N
         cerrarButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
@@ -89,7 +99,7 @@ public class Menu extends javax.swing.JFrame {
                 cerrarButtonMouseClicked(evt);
             }
         });
-        getContentPane().add(cerrarButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 0, 40, 40));
+        getContentPane().add(cerrarButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 0, 40, 40));
 
         MinButton.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/icons8_Expand_Arrow_32px.png"))); // NOI18N
         MinButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
@@ -98,10 +108,10 @@ public class Menu extends javax.swing.JFrame {
                 MinButtonMouseClicked(evt);
             }
         });
-        getContentPane().add(MinButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 0, 40, 40));
+        getContentPane().add(MinButton, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 0, 40, 40));
 
-        fondoLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/back.jpg"))); // NOI18N
-        getContentPane().add(fondoLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 425, 351));
+        fondoLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/back1.jpg"))); // NOI18N
+        getContentPane().add(fondoLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 360, 351));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -114,6 +124,18 @@ public class Menu extends javax.swing.JFrame {
         System.exit(0);
     }//GEN-LAST:event_cerrarButtonMouseClicked
 
+    private void jTextField1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField1ActionPerformed
+
+    private void jTextField3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField3ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField3ActionPerformed
+
+    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField2ActionPerformed
+
  
     public static void main(String args[]) {
    
@@ -125,14 +147,19 @@ public class Menu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel Covarianza;
-    private javax.swing.JLabel FuncionD;
-    private javax.swing.JLabel LimiteXY;
     private javax.swing.JLabel MinButton;
-    private javax.swing.JLabel PLimitesV;
     private javax.swing.JLabel ProyectoPYE;
-    private javax.swing.JLabel ValorE;
     private javax.swing.JLabel cerrarButton;
     private javax.swing.JLabel fondoLabel;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JRadioButton jRadioButton1;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
     // End of variables declaration//GEN-END:variables
 }
