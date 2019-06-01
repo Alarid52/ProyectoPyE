f7035b1d329da47c823b8c768159ee3e57a5afb4
Abraham <Abraham@DESKTOP-68M04UN>
2019-05-31 19:23:45 -0500

    prueba de nueva version

--- src/proyectopye/ProyectoPyE.java
+++ src/proyectopye/ProyectoPyE.java
@@ -8,9 +8,7 @@ public class ProyectoPyE {
     public static void main(String[] args) {
         Menu menu = new Menu();
         menu.setVisible(true);
-        //comentario para devolver version
+
     }
     
 }
-
-//comentario de prueba v2
